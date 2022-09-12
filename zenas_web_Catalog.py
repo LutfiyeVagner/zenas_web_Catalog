cc4f767c6de16932fef2f17d9e83a94cb0890e95
Lütfiye Vagner <112546204+LutfiyeVagner@users.noreply.github.com>
2022-09-12 19:35:14 +0200

    Update zenas_web_Catalog.py

--- zenas_web_Catalog.py
+++ zenas_web_Catalog.py
@@ -21,13 +21,11 @@ my_catalog = my_cur.fetchall()
 df = pandas.DataFrame(my_catalog) 
 
 # temp write the dataframe to the page so I Can see what I am working with 
-
-# streamlit.write(df) 
+streamlit.write(df) 
 
 # put the first column into a list 
 color_list = df[0].values.tolist() 
-
-# print(color_list) 
+print(color_list) 
 
 # Let's put a pick list here so they can pick the color 
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list)) 
@@ -40,4 +38,5 @@ my_cur.execute("select direct_url, price, size_list, upsell_product_desc from ca
 df2 = my_cur.fetchone() 
 streamlit.image( df2[0], width=400, caption= product_caption ) 
 streamlit.write('Price: ', df2[1]) 
-streamlit.write('Sizes Available: ',df2[2]) streamlit.write(df2[3]
+streamlit.write('Sizes Available: ',df2[2]) 
+streamlit.write(df2[3]
